6c52eaf89ff5f655e616df5f17863002c20e1121
Zachary_Theodore <fk9836@wayne.edu>
2018-07-19 13:43:50 -0400

    intial commit

---
+++ week1cap/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1cap
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Please Enter a Word for Translation: ");
+            string str = Console.ReadLine();
+
+            if (str[0] == 'a' || str[0] == 'e' || str[0] == 'i' || str[0] == 'o' || str[0] == 'u')
+            {
+                Console.WriteLine(str + "ay");
+            }
+
+            else
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (str[i] == 'a')
+                    {
+                        int vowelIndex = str.IndexOf('a');
+                        Console.Write(str.Substring(vowelIndex));
+                        Console.WriteLine(str.Substring(0, vowelIndex) + "ay");
+                        break;
+                    }
+                    else if (str[i] == 'e')
+                    {
+                        int vowelIndex = str.IndexOf('e');
+                        Console.Write(str.Substring(vowelIndex));
+                        Console.WriteLine(str.Substring(0, vowelIndex) + "ay");
+                        break;
+                    }
+                    else if (str[i] == 'i')
+                    {
+                        int vowelIndex = str.IndexOf('i');
+                        Console.Write(str.Substring(vowelIndex));
+                        Console.WriteLine(str.Substring(0, vowelIndex) + "ay");
+                        break;
+                    }
+                    else if (str[i] == 'o')
+                    {
+                        int vowelIndex = str.IndexOf('o');
+                        Console.Write(str.Substring(vowelIndex));
+                        Console.WriteLine(str.Substring(0, vowelIndex) + "ay");
+                        break;
+                    }
+                    else if (str[i] == 'u')
+                    {
+                        int vowelIndex = str.IndexOf('u');
+                        Console.Write(str.Substring(vowelIndex));
+                        Console.WriteLine(str.Substring(0, vowelIndex) + "ay");
+                        break;
+                    }
+                }
+                
+            }
+           
+           
+
+        }
+    }
+}

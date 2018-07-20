de15aaa70644598f96e1a291bb15a4ad1744a3a2
Zachary_Theodore <fk9836@wayne.edu>
2018-07-20 13:58:39 -0400

    added comments

--- week1cap/Program.cs
+++ week1cap/Program.cs
@@ -8,7 +8,7 @@ namespace week1cap
 {
     class Program
     {
-
+        //Method that recalls the program after it has run once
         static void Conti()
         {
             Console.WriteLine("Continue? (y/n)");
@@ -22,14 +22,16 @@ namespace week1cap
                 return;
             }
         }
-
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
         static void Main()
         {
+            //Takes The inputs and stores them into a string variable
             Console.WriteLine("Please Enter a Word for Translation: ");
             string str = Console.ReadLine();
             
-
+            //Searches for a first case being if the word starts with a vowel
+            //if so prints the word
             if (str[0] == 'a' || str[0] == 'e' || str[0] == 'i' || str[0] == 'o' || str[0] == 'u')
             {
                 Console.WriteLine(str + "way");
@@ -38,10 +40,15 @@ namespace week1cap
            
             else
             {
+                //THis whole section is where the magic happens.
                 for (int i = 0; i < str.Length; i++)
                 {
                     
-                    
+                    //for each of these if and else ifs the loop checks each index looking for 
+                    //a vowel, when it finds a vowel it remebers where that vowel is then
+                    // and prints from that vowel to the end of the string, then prints from
+                    //the 0 index up to right before the vowel, and then ay then breaks. 
+                  
                         if (str[i] == 'a')
                         {
                             int vowelIndex = str.IndexOf('a');
@@ -77,12 +84,19 @@ namespace week1cap
                             Console.WriteLine(str.Substring(0, vowelIndex) + "ay");
                             break;
                         }
-                    
-                    
+                        else if (str[i] == 'y')
+                        {
+                        int vowelIndex = str.IndexOf('y');
+                        Console.Write(str.Substring(vowelIndex));
+                        Console.WriteLine(str.Substring(0, vowelIndex) + "ay");
+                        break;
+                        }
+
                 }
                 
 
             }
+            //here we call the continue method OUTSIDE of the for loop.
             Conti();
 
         }
